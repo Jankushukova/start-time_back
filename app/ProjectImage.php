bea615f7e44dbed44819597033d76b25e5e30a37
Jankushukova <50518558+Jankushukova@users.noreply.github.com>
2020-05-19 12:45:28 +0600

    Update ProjectImage.php
    path added

--- app/ProjectImage.php
+++ app/ProjectImage.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProjectImage extends Model
 {
+    const PATH = 'images/project';
+
     protected $fillable = [
         'image',
         'product_id'
